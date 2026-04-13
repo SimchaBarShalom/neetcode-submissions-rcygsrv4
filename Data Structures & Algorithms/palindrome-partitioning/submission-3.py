cf03f968cb505f5e73f38264bd12ff31fa9a22ae
SimchaBS <simchabash84@gmail.com>
2026-04-14 00:39:07 +0300

    Add: palindrome-partitioning - submission-3

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res=[]
+        curr=[]
+        n=len(s)
+        dp=[[False]*(n) for i in range(n)]      
+        for end in range(n):
+            for start in range(end,-1,-1):
+                if s[start]==s[end] and (end-start<=2 or dp[start+1][end-1]):
+                    dp[start][end] = True
+                    
+        def subPalindrome(i):
+            if i>=n:
+                res.append(curr.copy())
+                return 
+            for j in range(i, n):
+                if dp[i][j]:
+                    curr.append(s[i:j+1])
+                    subPalindrome(j + 1)
+                    curr.pop()
+        subPalindrome(0)
+        return res
